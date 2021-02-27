8ec2ad60be89ca62aec0c5f896428467c5007455
Musa Al-hassy <alhassy@gmail.com>
2021-02-26 19:53:07 -0500

    Add on to the existing test setup ♥‿♥

--- tests.el
+++ tests.el
@@ -1,5 +1,5 @@
 (setq needed-libraries
-      '(s cl-lib dash org undercover))
+      '(s cl-lib dash org undercover seq quelpa))
 
 (require 'package)
 (push '("melpa" . "https://melpa.org/packages/") package-archives)
@@ -10,7 +10,21 @@
   (unless (package-installed-p pkg)
     (package-install pkg)))
 
-(undercover "*.el" (:report-format 'codecov))
+(defmacro deftest (desc &rest body)
+  `(ert-deftest ,(intern
+;; Convert all non-letters to ‘_’
+;; A = 65, z = 122
+(concat (seq-map (lambda (c) (if (<= 65 c 122) c ?_))
+         desc))) () ,@body))
+;; without the s-replace, “M-x ert” crashes when it comes to selecting the test to run.
+
+
+;; https://github.com/Wilfred/propcheck
+(quelpa '(propcheck :fetcher github :repo "Wilfred/propcheck"))
+(require 'propcheck)
+(when nil ;; example use
+  (let ((propcheck-seed (propcheck-seed)))
+    (propcheck-generate-string nil)))
 
 (load-file "holy-books.el")
 
